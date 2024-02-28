a90cda3dec98f3f09f0c048a3bb350e1449bfe31
pungkyhwa <pungky.hwa@gmail.com>
2024-02-28 09:51:26 +0700

    first commit

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Todolist::get();
+        foreach($data as $row){
+            echo $row->id ."<br>";
+            echo $row->kegiatan."<br>";
+            echo $row->tanggal."<br>";
+            echo $row->waktu."<br>";
+            echo $row->keterangan."<br>";
+            echo $row->status."<br>";
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_02_21_025207_create_todolists_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->id();
+            $table->string('kegiatan',100);
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->text('keterangan');
+            $table->string('status',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todolist');
+    }
+};
